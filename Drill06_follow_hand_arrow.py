de0b0bef49c8a5d8e59c82b76a503dc755465c68
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-30 02:39:34 +0900

    계획 및 왼클릭 이벤트 뼈대

--- Drill06_follow_hand_arrow.py
+++ Drill06_follow_hand_arrow.py
@@ -8,16 +8,22 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 arrow = load_image('hand_arrow.png')
 
+# 클릭한 위치에 화살표 만들어지고 소년 도착시 사라짐
+# 마우스 클릭 이벤트 추가
+# 클릭 좌표 저장 리스트 추가해서 for문으로 그려야겠다
 
 def handle_events():
     global running
-    global x, y
+    global x, y, points
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_MouseButtonEvent and event.button == SDL_BUTTON_LEFT:
+            #points.append(())
+
     pass
 
 
@@ -60,7 +66,7 @@ running = True
 is_collision = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 rx, ry = x, y
-
+points = []
 frame = 0
 direction = 1
 hide_cursor()
