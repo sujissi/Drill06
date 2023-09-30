62540c6a69be6258ee2fefe06aeb308206604cac
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-30 13:02:03 +0900

    커서따라 손 움직이게 완성
    클릭 좌표 저장은 아직 구현 중...

--- Drill06_follow_hand_arrow.py
+++ Drill06_follow_hand_arrow.py
@@ -15,23 +15,26 @@ arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y, points
+    global x, y, mx, my, points
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MouseButtonEvent and event.button == SDL_BUTTON_LEFT:
-    # points.append(())
+            points.append((event.x,  TUK_HEIGHT - 1 - event.y))
 
     pass
 
-def follow_arrow():
-    global x, y, rx, ry
+
+def follow_arrow(dest_x, dest_y):
+    global x, y
     t = 0.1
-    x = (1 - t) * x + t * rx
-    y = (1 - t) * y + t * ry
+    x = (1 - t) * x + t * dest_x
+    y = (1 - t) * y + t * dest_y
     pass
 
 
@@ -44,17 +47,18 @@ def is_collision(x1, y1, x2, y2):
 
 
 def set_img_dir():
-    global x, rx, direction
-    if x < rx:
-        direction = 1
-    else:
-        direction = 0
+    # global x, rx, direction
+    # if x < rx:
+    #     direction = 1
+    # else:
+    #     direction = 0
     pass
 
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-points = []
+mx, my = x, y
+points = [(x,y)]  # 화살표 좌표 저장
 frame = 0
 direction = 1
 hide_cursor()
@@ -62,11 +66,11 @@ hide_cursor()
 while running:
     clear_canvas()
     set_img_dir()
-    follow_arrow()
+    follow_arrow(points[0][0], points[0][1])
 
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * direction, 100, 100, x, y)
-
+    arrow.draw(mx, my)
     update_canvas()
     frame = (frame + 1) % 8
     delay(0.05)
